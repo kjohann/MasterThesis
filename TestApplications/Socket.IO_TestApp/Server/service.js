e8331cc21271d9ef7a6071eee8b5e0d9c5af8477
kjohann <kristian.johannessen.1990@gmail.com>
2013-03-15 20:45:38 +0100

    Added db calls in service.js

--- TestApplications/Socket.IO_TestApp/Server/service.js
+++ TestApplications/Socket.IO_TestApp/Server/service.js
@@ -1,39 +1,39 @@
 var database = require('./database')
 
 function verifyLogIn(username, password){
-
+    database.verifyLogIn(username, password);
 }
 
 function getBidsByUser(userId){
-
+    database.getBidsByUser(userId);
 }
 
 function placeBid(itemno, userId, value){
-
+    database.placeBid(itemno, userId, value);
 }
 
 function registerUser(username, firstname, lastname, adress, password){
-
+    database.registerUser(username, firstname, lastname, adress, password);
 }
 
-function registerItem(name, price, expires, description){
-
+function registerItem(name, price, expires, description, addedById){
+    database.registerItem(name, price, expires, description, addedById)
 }
 
 function deleteItem(itemno){
-
+    database.deleteItem(itemno);
 }
 
 function getAllItems(){
-
+    database.getAllItems();
 }
 
 function getLatestBid(bidId){
-
+    database.getLatestBid(bidId);
 }
 
 function getLatestItem(itemno){
-
+    database.getLatestItem((itemno));
 }
 
 exports.verifyLogIn = verifyLogIn;
