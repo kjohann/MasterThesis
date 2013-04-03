a34acc0f8d35b5934ca9069935494b055e50dd19
kjohann <kristian.johannessen.1990@gmail.com>
2013-04-03 16:13:25 +0200

    First method implemented in ServiceProvider

--- TestApplications/Lightstreamer_TestApp/src/data/service/ServiceProvider.java
+++ TestApplications/Lightstreamer_TestApp/src/data/service/ServiceProvider.java
@@ -1,11 +1,49 @@
 package data.service;
 
+import java.util.ArrayList;
+
+import models.*;
 import data.database.DatabaseHandler;
+import database.connector.Field;
+import database.connector.Row;
+
+/*TODO: Find out if I need synchronized*/
 
 public class ServiceProvider {
+	private static ServiceProvider instance;
 	private DatabaseHandler dbHandler;
+	private JSONHandler jsonHandler;
 	
-	public ServiceProvider(DatabaseHandler dbHandler){
+	private ServiceProvider(DatabaseHandler dbHandler){
 		this.dbHandler = dbHandler;
+		this.jsonHandler = JSONHandler.getInstance();
 	}
+	
+	public static ServiceProvider getInstance(DatabaseHandler dbHandler) {
+		return instance == null ? new ServiceProvider(dbHandler) : instance;
+	}
+	
+	public User verifyLogIn(String json) {
+		User logInUser = jsonHandler.userFromJSON(json);
+		if(logInUser == null) {
+			System.err.println("Error while verifying log in with data: " + json);
+			return null;
+		}
+		
+		ArrayList<Row> result = dbHandler.verifyLogIn(logInUser.getUsername(), logInUser.getPassword());
+		
+		if(result == null || result.size() > 1) //Should only return one user
+			return null;
+		
+		Row row = result.get(0);
+		Field userN = row.getField("Username"); Field id = row.getField("UserID");
+		Field firstN = row.getField("Firstname"); Field lastN = row.getField("Lastname"); 
+		Field adr = row.getField("Adress");
+		
+		String username = userN.getFieldAsString(), firstname = firstN.getFieldAsString(),
+			   lastname = lastN.getFieldAsString(), adress = adr.getFieldAsString();
+		int userId = id.getFieldAsInt();
+		
+		return new User(userId, username, firstname, lastname, adress, null); //password is not relevant to client
+	}			
 }
