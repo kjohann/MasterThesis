e521cb81f070cf502038fd9c008a40107471faea
kjohann <kristian.johannessen.1990@gmail.com>
2013-04-03 12:33:13 +0200

    Added all database logic

--- TestApplications/Lightstreamer_TestApp/src/data/database/DatabaseHandler.java
+++ TestApplications/Lightstreamer_TestApp/src/data/database/DatabaseHandler.java
@@ -1,5 +1,15 @@
 package data.database;
 
-public interface DatabaseHandler {
+import java.util.ArrayList;
 
+import database.connector.Row;
+
+public interface DatabaseHandler {
+	public abstract ArrayList<Row> verifyLogIn(String username, String password);
+	public abstract ArrayList<Row> getBidsByUser(String userId);
+	public abstract long placeBid(String itemno, String userId, String value, String username);
+	public abstract long registerUser(String username, String firstname, String lastname, String adress, String password);
+	public abstract long registerItem(String name, String price, String expires, String description, String addedByID);
+	public abstract boolean deleteItem(String itemno);
+	public abstract ArrayList<Row> getAllItems();
 }
--- TestApplications/Lightstreamer_TestApp/src/data/database/MySQLDatabaseHandler.java
+++ TestApplications/Lightstreamer_TestApp/src/data/database/MySQLDatabaseHandler.java
@@ -1,8 +1,59 @@
 package data.database;
 
+import java.util.ArrayList;
+
 import data.database.resultGenerators.ResultGeneratorFactory;
-import database.connector.ResultGenerator;
+import database.connector.*;
 
 public class MySQLDatabaseHandler implements DatabaseHandler {
+	private Connector mysqlConnector; //See Utils folder
+	private ResultGeneratorFactory generatorFactory;
 	
+	public MySQLDatabaseHandler(String dbName, String user, String password) {
+		mysqlConnector = MySQLConnector.getInstance(dbName);
+		mysqlConnector.init(user, password);
+		generatorFactory = ResultGeneratorFactory.getInstance();
+	}
+
+	@Override
+	public ArrayList<Row> verifyLogIn(String username, String password) {
+		String query = QueryStore.getVerifyLogInQuery(username, password);
+		return mysqlConnector.select(query, generatorFactory.getVerifyLogInGenerator());		
+	}
+
+	@Override
+	public ArrayList<Row> getBidsByUser(String userId) {
+		String query = QueryStore.getBidsByUserQuery(userId);
+		return mysqlConnector.select(query, generatorFactory.getBidsByUserGenerator());
+	}
+
+	@Override
+	public long placeBid(String itemno, String userId, String value, String username) {
+		String query = QueryStore.getPlaceBidQuery(itemno, userId, value, username);
+		return mysqlConnector.insert(query);
+	}
+
+	@Override
+	public long registerUser(String username, String firstname, String lastname, String adress, String password) {
+		String query = QueryStore.getRegisterUserQuery(username, firstname, lastname, adress, password);
+		return mysqlConnector.insert(query);
+	}
+
+	@Override
+	public long registerItem(String name, String price, String expires, String description, String addedByID) {
+		String query = QueryStore.getRegisterItemQuery(name, price, expires, description, addedByID);
+		return mysqlConnector.insert(query);
+	}
+
+	@Override
+	public boolean deleteItem(String itemno) {
+		String query = QueryStore.getDeleteItemQuery(itemno);
+		return mysqlConnector.delete(query);
+	}
+
+	@Override
+	public ArrayList<Row> getAllItems() {
+		String query = QueryStore.getAllItemsQuery();
+		return mysqlConnector.select(query, generatorFactory.getAllItemsGenerator());
+	}	
 }
