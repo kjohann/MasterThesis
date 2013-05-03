a8156e05bf8992236692fc455057c2d01ba20983
kjohann <kristian.johannessen.1990@gmail.com>
2013-05-03 16:27:44 +0200

    Some template rendering tests

---
+++ TestApplications/Playframework_TestApp/test/models/TemplateTest.java
@@ -0,0 +1,38 @@
+package models;
+
+import org.junit.*;
+
+import play.mvc.Content;
+import static org.junit.Assert.*;
+import static play.test.Helpers.*;
+
+public class TemplateTest {
+	
+	@Test
+	public void hasWSTest() {
+		Content html = views.html.index.render(true);
+		assertEquals("text/html", contentType(html));
+		assertTrue(contentAsString(html).contains("wsInit.js"));
+		assertTrue(contentAsString(html).contains("websocket.js"));
+		assertFalse(contentAsString(html).contains("cometInit.js"));
+		assertFalse(contentAsString(html).contains("comet.js"));
+	}
+	
+	@Test
+	public void noWSTest() {
+		Content html = views.html.index.render(false);
+		assertEquals("text/html", contentType(html));
+		assertTrue(contentAsString(html).contains("cometInit.js"));
+		assertTrue(contentAsString(html).contains("comet.js"));
+		assertFalse(contentAsString(html).contains("wsInit.js"));
+		assertFalse(contentAsString(html).contains("websocket.js"));
+	}
+	
+	@Test
+	public void hasCorrectTitle() {
+		Content html = views.html.index.render(false);
+		assertTrue(contentAsString(html).contains("Auction House"));
+		html = views.html.index.render(true);
+		assertTrue(contentAsString(html).contains("Auction House"));
+	}
+}
