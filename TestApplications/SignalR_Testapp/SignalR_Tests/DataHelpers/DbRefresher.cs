65d8d1590a95c8f3a6297831ff53028ca941dcc4
kjohann <kristian.johannessen.1990@gmail.com>
2013-05-14 18:00:48 +0200

    Started writing Selenium tests

---
+++ TestApplications/SignalR_Testapp/SignalR_Tests/DataHelpers/DbRefresher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SignalR_Testapp.Database;
+
+namespace SignalR_Tests.DataHelpers
+{
+    public class DbRefresher
+    {
+        public static bool refresh()
+        {
+            try
+            {
+                var path = "C:\\Users\\Kristian\\Documents\\GitHub\\MasterThesis\\TestApplications\\" +
+                           "SignalR_Testapp\\SignalR_Tests\\DataHelpers\\RestoreDb.sql";
+                var sql = File.ReadAllText(path);
+                var db = new auctionhouseEntities();
+                db.Database.ExecuteSqlCommand(sql);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ TestApplications/SignalR_Testapp/SignalR_Tests/Integrationtests/IntegrationTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using Selenium;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SignalR_Tests.Integrationtests
+{
+    [TestFixture]
+    public class IntegrationTest
+    {
+        //private ISelenium _selenium;
+        private const string _url = "http://localhost:55143/Views/index.html";
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void setUp()
+        {
+            DataHelpers.DbRefresher.refresh();
+            _driver = new FirefoxDriver();
+            _driver.Navigate().GoToUrl(_url);
+            _driver.FindElement(By.LinkText("Log in")).Click();
+            _driver.FindElement(By.CssSelector("#log_usern")).SendKeys("Mozilla");
+            _driver.FindElement(By.CssSelector("#log_pass")).SendKeys("123");
+            _driver.FindElement(By.CssSelector("#log_in_button")).Click();
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(3));
+            wait.Until((d) => d.FindElement(By.LinkText("Mozilla")).Text.Equals("Mozilla"));
+        }
+
+        [Test]
+        public void titleTest()
+        {
+            Assert.AreEqual("Auction House", _driver.Title);
+        } 
+
+        [Test]
+        public void canGetInitialItemsTest()
+        {
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(3));
+            wait.Until((d) => d.FindElements(By.CssSelector(".item")).Count == 3);
+        }
+
+        [Test]
+        public void canAddItem()
+        {
+            _driver.FindElement((By.CssSelector("#addItemButton"))).Click();
+            _driver.FindElement(By.CssSelector("#itemname")).SendKeys("Selenium");
+            _driver.FindElement(By.CssSelector("#minprice")).SendKeys("1337");
+            _driver.FindElement(By.CssSelector("#expires")).SendKeys("2014-03-12");
+            _driver.FindElement(By.CssSelector("#description")).SendKeys("This was added by an automated test.");
+            _driver.FindElement(By.CssSelector(("#addButton"))).Click();
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(3));
+            wait.Until((d) => d.FindElements(By.CssSelector(".item")).Count == 4);
+        }
+
+        [Test]
+        public void canPlaceBid()
+        {
+            _driver.FindElements(By.CssSelector(".bidButton"))[0].Click();
+            _driver.FindElement(By.CssSelector("#bid")).SendKeys("450000");
+            _driver.FindElement(By.CssSelector("#place_bid_button")).Click();
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(3));
+            wait.Until((d) => d.FindElements(By.XPath("span [text() = \"450000\"]")).Count == 1);
+        }
+
+        [TearDown]
+        public void tearDown()
+        {
+            try
+            {
+                _driver.Quit();
+                DataHelpers.DbRefresher.refresh();
+            }
+            catch (Exception e)
+            {                
+                
+            }
+        }
+    }
+}
