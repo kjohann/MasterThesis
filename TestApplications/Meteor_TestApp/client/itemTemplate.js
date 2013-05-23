7b08ed8f52eb55d53507186374c375636fdc891f
kjohann <kristian.johannessen.1990@gmail.com>
2013-05-23 21:13:46 +0200

    Refactored view a little + added func for visibility of removebutton

--- TestApplications/Meteor_TestApp/client/itemTemplate.js
+++ TestApplications/Meteor_TestApp/client/itemTemplate.js
@@ -1,7 +1,10 @@
 $.extend(Template.itemcontainer, {
  	items: function() {
  		return Items.find({});
- 	}
+ 	},
+ 	loggedIn: function() {
+		return Template.content.loggedIn();
+	}
 });
 
 $.extend(Template.item, {
@@ -13,6 +16,9 @@ $.extend(Template.item, {
 	},
 	itemno: function() {
 		return this._id.substring(0,5); //this is a bit weird since i use MangoDb instead of MySql
+	},
+	removeVisible: function () {
+		return Session.get("User").username === this.addedBy ? "removeVisible" : "removeInvisible";
 	},
 	events: {
 		"click .bidButton": function() {
