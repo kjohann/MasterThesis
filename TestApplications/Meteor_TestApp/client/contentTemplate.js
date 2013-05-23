4e4401f4350a8703f516db620f9e324cb8c6d3f7
kjohann <kristian.johannessen.1990@gmail.com>
2013-05-23 12:58:11 +0200

    Switched class names for visibility of login related stuff

--- TestApplications/Meteor_TestApp/client/contentTemplate.js
+++ TestApplications/Meteor_TestApp/client/contentTemplate.js
@@ -2,9 +2,9 @@
 
  $.extend(Template.content, {
  	loggedIn: function() {
- 		return Session.equals("logged_in", true) ? "log_in_visibile" : 'log_in_invisible';
+ 		return Session.equals("logged_in", true) ? "log_in_invisible" : 'log_in_visible';
  	},
  	notLoggedIn: function() {
- 		return !(Session.equals("logged_in", true)) ? "log_in_visibile" : 'log_in_invisible';	
+ 		return !(Session.equals("logged_in", true)) ? "log_in_invisible" : 'log_in_visible';	
  	}
  });
