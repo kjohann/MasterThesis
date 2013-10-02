73c54c7a26d045131f51d1f3c26ec82b2e566f84
kjohann <kristian.johannessen.1990@gmail.com>
2013-10-02 12:22:42 +0200

    Added support for passing command line arguments to testcase

--- TestApplications/FunctionalTestCase/src/test/java/auctionhouse/FunctionalTest.java
+++ TestApplications/FunctionalTestCase/src/test/java/auctionhouse/FunctionalTest.java
@@ -2,6 +2,8 @@ package auctionhouse;
 
 import static org.junit.Assert.*;
 
+import javax.swing.JOptionPane;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
@@ -10,9 +12,23 @@ import org.junit.Test;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class FunctionalTest {
-
+	private static String url = "";
+	//run with mvn test -DargLine="-Dhost=[host] -Dport=[port] -DpageUrl=[(optional)url]" 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		String host = System.getProperty("host");
+		assertNotNull("Need to pass host parameter! Use -DargLine=\"-Dhost=[host]\"", host);
+		url+= host;
+		
+		
+		String port = System.getProperty("port");
+		assertNotNull("Need to pass port parameter! Use -DargLine=\"-Dport=[port]\"", port);
+		url += ":" + port;
+		
+		String pageUrl = System.getProperty("pageUrl");
+		if(pageUrl != null) {
+			url += "/" + pageUrl;
+		}
 	}
 
 	@AfterClass
@@ -21,7 +37,7 @@ public class FunctionalTest {
 
 	@Test
 	public void test() {
-		assertEquals(1, 1);
+		assertEquals("localhost:80/test/", url);
 	}
 
 }
