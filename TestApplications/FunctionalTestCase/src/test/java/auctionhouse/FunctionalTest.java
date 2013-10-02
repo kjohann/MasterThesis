817026cbc3e0a5f52adfd9524cbb5c239905525b
kjohann <kristian.johannessen.1990@gmail.com>
2013-10-02 16:51:43 +0200

    Made test steps for register and log in

--- TestApplications/FunctionalTestCase/src/test/java/auctionhouse/FunctionalTest.java
+++ TestApplications/FunctionalTestCase/src/test/java/auctionhouse/FunctionalTest.java
@@ -8,8 +8,14 @@ import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.runners.MethodSorters;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import com.opera.core.systems.OperaDriver;
 import database.connector.*;
 
@@ -43,12 +49,58 @@ public class FunctionalTest {
 	}
 
 	@Test
-	public void test1_title_in_both_browsers_should_be_Auction_House() {		
+	public void step1_is_title_in_both_browsers_should_be_Auction_House() {		
 		String firefoxTitle = firefox.getTitle();
 		String operaTitle = opera.getTitle();
 		
 		assertEquals("Auction House", firefoxTitle);
 		assertEquals(firefoxTitle, operaTitle);
+	}
+	
+	@Test
+	public void step2_is_user_should_be_able_to_register_new_user() {
+		find(firefox, By.linkText("Register")).click();
+
+		fill(find(firefox, By.id("firstname")), "Kristian");
+		fill(find(firefox, By.id("lastname")), "Johannessen");
+		fill(find(firefox, By.id("adress")), "123 Fakestreet");
+		fill(find(firefox, By.id("username")), username);
+		fill(find(firefox, By.id("password")), "123");
+		
+		find(firefox, By.id("register_button")).click();
+		(new WebDriverWait(firefox, 3)).until(new ExpectedCondition<Boolean>() {
+
+			@Override
+			public Boolean apply(WebDriver arg0) {
+				Alert alert = firefox.switchTo().alert();
+				if(alert != null) {
+					alert.accept();
+					return true;
+				} else {
+					return false;
+				}				
+			}
+		
+		});					
+	}
+	
+	@Test
+	public void step3_is_user_should_be_able_to_log_in_with_registered_user() {
+		find(firefox, By.linkText("Log in")).click();
+		
+		fill(find(firefox, By.id("log_usern")), username);
+		fill(find(firefox, By.id("log_pass")), "123");
+		
+		find(firefox, By.id("log_in_button")).click();
+		
+		(new WebDriverWait(firefox, 3)).until(new ExpectedCondition<Boolean>() {
+
+			@Override
+			public Boolean apply(WebDriver arg0) {
+				return find(firefox, By.linkText(username)).isDisplayed();		
+			}
+		
+		});	
 	}
 	
 	private static void verifyUrl() {
@@ -85,6 +137,14 @@ public class FunctionalTest {
 	private static void initDatabase() {
 		connector = MySQLConnector.getInstance("test");
 		assertTrue(connector.init("n5user", "n5pass"));				
+	}
+	
+	private WebElement find(WebDriver browser, By criteria) {
+		return browser.findElement(criteria);
+	}
+	
+	private void fill(WebElement element, String with) {
+		element.sendKeys(with);
 	}
 
 }
