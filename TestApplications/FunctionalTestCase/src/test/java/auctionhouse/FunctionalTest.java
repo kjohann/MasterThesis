c12acb31b8c9966e720e42e2ac7ee9a3ff86c7a6
kjohann <kristian.johannessen.1990@gmail.com>
2013-10-02 14:20:35 +0200

    Added setup for browsers and test for title

--- TestApplications/FunctionalTestCase/src/test/java/auctionhouse/FunctionalTest.java
+++ TestApplications/FunctionalTestCase/src/test/java/auctionhouse/FunctionalTest.java
@@ -9,10 +9,16 @@ import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.runners.MethodSorters;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.opera.core.systems.OperaDriver;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class FunctionalTest {
 	private static String url = "";
+	private static WebDriver opera, firefox;
+	
 	//run with mvn test -DargLine="-Dhost=[host] -Dport=[port] -DpageUrl=[(optional)url]" 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -29,15 +35,29 @@ public class FunctionalTest {
 		if(pageUrl != null) {
 			url += "/" + pageUrl;
 		}
+		
+		opera = new OperaDriver();
+		firefox = new FirefoxDriver();
+		
+		firefox.navigate().to(url);
+		opera.navigate().to(url);			
+		
+		System.out.println("Title: " + firefox.getTitle());
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
+		opera.quit();
+		firefox.quit();
 	}
 
 	@Test
-	public void test() {
-		assertEquals("localhost:80/test/", url);
+	public void test1_title_in_both_browsers_should_be_Auction_House() {		
+		String firefoxTitle = firefox.getTitle();
+		String operaTitle = opera.getTitle();
+		
+		assertEquals("Auction House", firefoxTitle);
+		assertEquals(firefoxTitle, operaTitle);
 	}
 
 }
