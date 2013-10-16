534b7f1d784fcc8641e688db7faf503d90cf1b6b
kjohann <kristian.johannessen.1990@gmail.com>
2013-10-16 17:07:16 +0200

    Started refactoring unit tests (verifyLogIn)

--- TestApplications/SignalR_Testapp/SignalR_Tests/Unittests/AuctionHubServiceTest.cs
+++ TestApplications/SignalR_Testapp/SignalR_Tests/Unittests/AuctionHubServiceTest.cs
@@ -1,99 +1,103 @@
-﻿using System;
+﻿using Moq;
 using NUnit.Framework;
-using SignalR_Tests.Unittests;
+using SignalR_Testapp.Database;
 using SignalR_Testapp.Hubs;
 using SignalR_Testapp.Models;
-using System.Collections.Generic;
-using SignalR_Testapp.Database;
 
-namespace SignalR_Tests
+namespace SignalR_Tests.Unittests
 {
     [TestFixture]
     public class AuctionHubServiceTest
     {
-        private IDataprovider _provider;
         private AuctionHubService _service;
+        private Mock<IDataprovider> _providerMock;
 
         [SetUp]
-        public void init()
+        public void SetUp()
         {
-            _provider = new FakeDataprovider();
-            _service = new AuctionHubService(_provider);
+            _providerMock = new Mock<IDataprovider>();
         }
 
         [Test]
-        public void verifyLoginTest()
+        public void VerifyLogin_should_return_a_user_if_successful()
         {
-            User successUser = _service.verifyLogin("User1", "123");
-            Assert.NotNull(successUser);
-            Assert.AreEqual("User1", successUser.username);
-            Assert.AreEqual(1, successUser.userID);
-
-            User failUser = _service.verifyLogin("User1", "wrong");
-            Assert.Null(failUser);
-            failUser = _service.verifyLogin("Wrong", "123");
-            Assert.Null(failUser);
+            _providerMock.Setup(x => x.VerifyLogin(It.IsAny<string>(), It.IsAny<string>())).Returns(new User() {userID = 1, firstname = "Testname", username = "Testuser"});
+            _service = new AuctionHubService(_providerMock.Object);
+            var result = _service.VerifyLogin("TestUser", "123");
+            Assert.NotNull(result);
+            Assert.AreEqual("Testname", result.firstname);
+            //User failUser = _service.VerifyLogin("User1", "wrong");
+            //Assert.Null(failUser);
+            //failUser = _service.VerifyLogin("Wrong", "123");
+            //Assert.Null(failUser);
         }
 
         [Test]
-        public void getAllItemsTest()
+        public void VerifyLogin_should_return_null_if_unsuccessful()
         {
-            IEnumerable<PrettyItem> items = _service.getAllItems();
-            Assert.AreEqual(3, count(items));
+            _service = new AuctionHubService(_providerMock.Object);
+            Assert.Null(_service.VerifyLogin("Care", "Face"));
         }
 
-        [Test]
-        public void getUsersBidsTest()
-        {
-            IEnumerable<ViewBid> bids = _service.getUsersBids(1);
-            Assert.AreEqual(1, count(bids));
-            bids = _service.getUsersBids(2);
-            Assert.AreEqual(1, count(bids));
-        }
+        //[Test]
+        //public void getAllItemsTest()
+        //{
+        //    IEnumerable<PrettyItem> items = _service.GetAllItems();
+        //    Assert.AreEqual(3, count(items));
+        //}
 
-        [Test]
-        public void registerTest()
-        {
-            User register = new User { username = "User4", firstname = "Insert", lastname = "Insertson", adress = "Insstreet", password = "qwe" };
-            Assert.True(_service.register(register));
-            Assert.AreEqual(4, register.userID);
-            Assert.False(_service.register(null));
-        }
+        //[Test]
+        //public void getUsersBidsTest()
+        //{
+        //    IEnumerable<ViewBid> bids = _service.GetUsersBids(1);
+        //    Assert.AreEqual(1, count(bids));
+        //    bids = _service.GetUsersBids(2);
+        //    Assert.AreEqual(1, count(bids));
+        //}
 
-        [Test]
-        public void addItemTest()
-        {
-            Item addItem = new Item { name = "InsertItem", price = 1337, expires = new DateTime(2015, 10, 21), description = "What movie reference?", addedByID = 3 };
-            PrettyItem item = _service.addItem(addItem, "User3");
-            Assert.AreEqual(4, addItem.itemno);
-            Assert.AreEqual(4, item.itemno);
-            Assert.AreEqual(1337, item.price);
-            Assert.AreEqual("User3", item.highestBidder);
-            Assert.Null(_service.addItem(null, "User3"));
-        }
+        //[Test]
+        //public void registerTest()
+        //{
+        //    User register = new User { username = "User4", firstname = "Insert", lastname = "Insertson", adress = "Insstreet", password = "qwe" };
+        //    Assert.True(_service.Register(register));
+        //    Assert.AreEqual(4, register.userID);
+        //    Assert.False(_service.Register(null));
+        //}
 
-        [Test]
-        public void deleteItemTest()
-        {
-            Assert.True(_service.deleteItem(1));
-            Assert.False(_service.deleteItem(4));
-        }
+        //[Test]
+        //public void addItemTest()
+        //{
+        //    Item addItem = new Item { name = "InsertItem", price = 1337, expires = new DateTime(2015, 10, 21), description = "What movie reference?", addedByID = 3 };
+        //    PrettyItem item = _service.AddItem(addItem, "User3");
+        //    Assert.AreEqual(4, addItem.itemno);
+        //    Assert.AreEqual(4, item.itemno);
+        //    Assert.AreEqual(1337, item.price);
+        //    Assert.AreEqual("User3", item.highestBidder);
+        //    Assert.Null(_service.AddItem(null, "User3"));
+        //}
 
-        [Test]
-        public void placeBidTest()
-        {
-            Bid bid = new Bid { itemno = 3, userID = 3, username = "User3", value = 400000 };
-            Bid newbid = _service.placeBid(bid);
+        //[Test]
+        //public void deleteItemTest()
+        //{
+        //    Assert.True(_service.DeleteItem(1));
+        //    Assert.False(_service.DeleteItem(4));
+        //}
 
-            Assert.AreEqual(bid, newbid);
-            Assert.AreEqual(5, bid.bidID);
-        }
+        //[Test]
+        //public void placeBidTest()
+        //{
+        //    Bid bid = new Bid { itemno = 3, userID = 3, username = "User3", value = 400000 };
+        //    Bid newbid = _service.PlaceBid(bid);
 
-        private int count(IEnumerable<object> list)
-        {
-            int i = 0;
-            foreach (var o in list) { i++; }
-            return i;
-        }
+        //    Assert.AreEqual(bid, newbid);
+        //    Assert.AreEqual(5, bid.bidID);
+        //}
+
+        //private int count(IEnumerable<object> list)
+        //{
+        //    int i = 0;
+        //    foreach (var o in list) { i++; }
+        //    return i;
+        //}
     }
 }
