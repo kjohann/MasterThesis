d037ff9ec6a2208d9b1c5dd9f9c8127c2a256f68
kjohann <kristian.johannessen.1990@gmail.com>
2013-10-29 16:35:03 +0100

    Added a monitor class to keep track of data on the server

---
+++ Loadtests/SignalRLoad/SignalRLoad/Models/Monitor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRLoad.Models
+{
+    public class Monitor
+    {
+        private static Monitor _instance;
+        public int NumberOfClients { get; set; }
+        public int ExpectedTestDurationInMillis { get; set; }
+        public long MessagesReceived { get; set; }
+        public long MessagesSent { get; set; }
+        public IEnumerable<string> CompletedClients { get; set; } 
+
+        private Monitor() {}
+
+        public static Monitor GetInstance()
+        {
+            return _instance ?? (_instance = new Monitor());
+        }
+
+        public bool Complete()
+        {
+            return CompletedClients.Count() == NumberOfClients;
+        }
+
+        public int OverTime(int actualTimeInMillis)
+        {
+            return actualTimeInMillis - ExpectedTestDurationInMillis;
+        }
+    }
+}
