fe2aec709db081efbca8c69c064175d42c62e2d7
kjohann <kristian.johannessen.1990@gmail.com>
2013-10-29 15:43:45 +0100

    Skeleton for hub - need to find what to send back and forth in tests

--- Loadtests/SignalRLoad/SignalRLoad/Hubs/LoadHub.cs
+++ Loadtests/SignalRLoad/SignalRLoad/Hubs/LoadHub.cs
@@ -8,9 +8,32 @@ namespace SignalRLoad.Hubs
 {
     public class LoadHub : Hub
     {
+        //Remove this..
         public void Hello()
         {
             Clients.All.hello();
         }
+
+        public void InitTest(String testToRun, int numberOfClients)
+        {
+            //save numberOfClients
+            Clients.All.initTest(testToRun);
+        }
+
+        //Echo and EchoBroadCast
+
+        public void Complete(String clientID)
+        {
+            //register completed
+            //if all are completed
+            Clients.All.harvest(); //getData
+        }
+
+        public void GetData( /*Some object containting data*/)
+        {
+            //merge data into one entity and write to file (csv or something)
+            Clients.All.harvestComplete( /*Dataobject (as JSON if needed)*/); //only the "master client" will get this.
+        }
+
     }
 }
