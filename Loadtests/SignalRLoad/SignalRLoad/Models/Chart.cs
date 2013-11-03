24e9d5e2153385d43ea7483bd1b7daed55afe3f5
kjohann <kristian.johannessen.1990@gmail.com>
2013-11-03 13:28:28 +0100

    Added class to represent data for HighCharts

---
+++ Loadtests/SignalRLoad/SignalRLoad/Models/Chart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRLoad.Models
+{
+    public class Chart
+    {
+        public string Title { get; set; }
+        public string[] XAxis { get; set; }
+        public string[] YAxis { get; set; }
+        public string YAxisTitle { get; set; }
+    }
+}
