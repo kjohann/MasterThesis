b7731abb991d22264d236281046c66aa0adca6f6
kjohann <kristian.johannessen.1990@gmail.com>
2013-11-05 16:09:30 +0100

    Alterations in ClientFunctions to try to get harvest complete work

--- Loadtests/SignalRLoad/SignalRLoad/Scripts/ClientFunctions.js
+++ Loadtests/SignalRLoad/SignalRLoad/Scripts/ClientFunctions.js
@@ -1,38 +1,46 @@
 ﻿(function(loadTest, root, charts) {
     root.receiveMessage = function (message) { 
-        findClient(message.ClientId).done(function (foundClient) {
+        console.log("receied message on client " + message.ClientId);
+        root.findClient(message.ClientId).done(function (foundClient) {
+            console.log("Aaaand found client");
             message.ReceivedAtClient = new Date();
             foundClient.messages.push(message);
         }).fail(function (error) { }); //really just ignore
     };
 
     root.harvestComplete = function(data) {
-        findClient(loadTest.masterId).done(function(foundClient) {
-            charts.getCharts(data); //only invoke for master client
-        }).fail(function (error){ });        
+        var masterId = loadTest.masterId;
+        while (loadTest.masterId != 0) {
+            console.log("Harvest complete");
+            loadTest.masterId = 0;
+            root.findClient(masterId).done(function (foundClient) {
+                charts.getCharts(data); //only invoke for master client
+            }).fail(function(error) {
+            });
+        }
     };
 
     root.promoteToMaster = function (clientId) { 
-        findClient(clientId).done(function (foundClient) {
+        root.findClient(clientId).done(function (foundClient) {
             foundClient.master = true;
             loadTest.masterId = clientId;
             console.log("Promoted client with id " + foundClient.clientId + " to master");
         }).fail(clientNotFound);
     };
-    
-    function findClient(clientId) { 
+
+    root.findClient = function(clientId) {
         var deferred = new $.Deferred();
 
-        $.each(loadTest.clients, function (index, currentClient) {
-            if (currentClient.clientId === clientId) {
+        $.each(loadTest.clients, function(index, currentClient) {
+            if (currentClient.clientId == clientId) {
                 deferred.resolve(currentClient);
-            } else if (index === loadTest.clients.length - 1) {
+            } else if (index == loadTest.clients.length - 1) {
                 deferred.reject({ message: "Couldn't find client with id: " + clientId });
             }
         });
 
         return deferred.promise();
-    }
+    };
 
     function clientNotFound(error) { 
         console.log(error.message);
