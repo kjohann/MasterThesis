b2fbe499b53a09b26a335376d15c2d500d522300
kjohann <kristian.johannessen.1990@gmail.com>
2013-11-05 16:10:19 +0100

    Forgot to add start function and trying to get sendMessage to work properly
    Sends too many!

--- Loadtests/SignalRLoad/SignalRLoad/Scripts/Communication.js
+++ Loadtests/SignalRLoad/SignalRLoad/Scripts/Communication.js
@@ -5,10 +5,10 @@
             var connection = $.hubConnection();
             var hubProxy = connection.createHubProxy('loadHub');
 
-            hubProxy.on('initTest', initTest);
+            hubProxy.on('initTest', root.initTest);
             hubProxy.on('receiveEcho', functions.receiveMessage);
             hubProxy.on('receiveBroadcast', functions.receiveMessage);
-            hubProxy.on('harvest', harvest);
+            hubProxy.on('harvest', root.harvest);
             hubProxy.on('harvestComplete', functions.harvestComplete);
 
             loadTest.clients.push(new loadTest.Client(clientId, hubProxy));
@@ -21,14 +21,20 @@
         }
     };
 
+    root.start = function(test) {
+        functions.findClient(loadTest.masterId).done(function(client) {
+            var testDuration = loadTest.numberOfMessages * loadTest.messageInterval;
+            client.socket.invoke('initTest', test, loadTest.numberOfClientsTotal, testDuration);
+        }).fail(function (error){ });
+    };
+    var a = 0;
     root.initTest = function(test) { 
-        $.each(loadTest.clients, function(index, currentClient) {
-            if (test === 'echo' || test === 'broadcast') {
-                sendMessage(test, currentClient);
-            } else {
-                console.error("No such test!");
-            }
-        });
+        if (test === 'echo' || test === 'broadcast' && a++ < 1) {
+            console.log("Initializing");
+            sendMessages(test);
+        } else {
+            console.error("No such test!");
+        }
     }; 
 
     root.harvest = function() {
@@ -38,10 +44,19 @@
         });
     };
     
-    function sendMessage(test, client) {
-        if (client.messagesSent < loadTest.numberOfMessages) {
-            client.socket.invoke(test, new loadTest.Message("1337", client.clientId));
-            setTimeout(sendMessage, loadTest.messageInterval);
-        }
+    function sendMessages(test) {
+        $.each(loadTest.clients, function (index, client) {
+            if (client.messagesSent < loadTest.numberOfMessages) {
+                client.messagesSent++;
+                console.log("Sending message from client " + client.clientId + " time: " + new Date().toString());
+                client.socket.invoke(test, new loadTest.Message("1337", client.clientId));
+                setTimeout(function() {
+                     sendMessages(test);
+                }, loadTest.messageInterval );
+            } else {
+                console.log("Sending complete for client " + client.clientId);
+                client.socket.invoke('complete', client.clientId);
+            }
+        });
     }
 })(loadTest, loadTest.communications = loadTest.communications || {}, loadTest.clientFunctions);
