830c65f2501f3f940c69faea42d62c028bbddb67
kjohann <kristian.johannessen.1990@gmail.com>
2013-11-05 16:22:07 +0100

    Forgot to commit extension

---
+++ Loadtests/SignalRLoad/SignalRLoad/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRLoad.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static long ToMilliseconds(this DateTime date)
+        {
+            return (long) (date - new DateTime(1970, 1, 1)).TotalMilliseconds;
+        }
+    }
+}
