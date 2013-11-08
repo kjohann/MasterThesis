8533e8c92af56d8f2cf36cde96d31c9fb485825a
kjohann <kristian.johannessen.1990@gmail.com>
2013-11-08 18:22:14 +0100

    Added chart!

--- Loadtests/SignalRLoad/SignalRLoad/Scripts/Charts.js
+++ Loadtests/SignalRLoad/SignalRLoad/Scripts/Charts.js
@@ -1,13 +1,49 @@
-﻿(function(root) { //will have other dependencies as well
-    //bind methods from server that are needed to build charts
-    root.getCharts = function(data) { //in charts somehow
-        //get charts from api
+﻿(function(root) { 
+    root.getCharts = function(data) {        
         data.Type = "Messages";
         console.log(data);
-        //var json = JSON.stringify(data);
         $.post(loadTest.options.chartUrl, data).done(function(chart) {
-            console.log(chart);
+            $("#messagesChart").highcharts(getChart(chart));
         });
-        //display charts
     };
+    
+    function getChart(chart) {
+        return {
+            title: {
+                text: chart.Title,
+                x: -20
+            },
+            xAxis: {
+                categories: chart.XAxis
+            },
+            yAxis: {
+                title: {
+                    text: chart.YAxisTitle
+                },
+                plotLines: [{
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle',
+                borderWidth: 0
+            },
+            series: mapSeries(chart)
+    };
+    }
+
+    function mapSeries(chart) {
+        return $.map(chart.Series, function (value, index) {
+            return {
+                data: $.map(value.Data, function (val, i) {
+                    return parseFloat(val);
+                }),
+                name: value.Name
+            };
+        });
+    }
+
 })(loadTest.charts = loadTest.charts || {});
