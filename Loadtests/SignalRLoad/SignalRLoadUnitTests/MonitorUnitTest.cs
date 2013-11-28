c11d0ba7852a47138b84cae9096e1090298e13b3
kjohann <kris-90@live.no>
2013-11-28 13:07:53 +0100

    Wrote tests for RegisterSentFromClientEvent

--- Loadtests/SignalRLoad/SignalRLoadUnitTests/MonitorUnitTest.cs
+++ Loadtests/SignalRLoad/SignalRLoadUnitTests/MonitorUnitTest.cs
@@ -16,6 +16,7 @@ using FluentAssertions;
 //outVal2 = test[1];
 //outVal1.ShouldBeEquivalentTo(1338);
 //outVal2.ShouldBeEquivalentTo(2);                
+using SignalRLoad.Extensions;
 using SignalRLoad.Models;
 
 namespace SignalRLoadUnitTests
@@ -37,16 +38,54 @@ namespace SignalRLoadUnitTests
         [Test]
         public void RegisterSentFromClientEvent_should_register_an_event_within_the_correct_interval()
         {
+            var values = GetDummyMillisecondValues(200, 20);
+            RegisterSentFromClientEvents(values);
 
+            var expectedData = new[] {4, 5, 5, 5, 1};
+
+            _monitor.SentFromClientEvents.ShouldAllBeEquivalentTo(expectedData);
+        }
+
+        [Test]
+        public void RegisterSentFromClientEvent_should_register_an_event_also_with_different_spacing()
+        {
+            var values = GetDummyMillisecondValues(200, 40);
+            RegisterSentFromClientEvents(values, 5);
+
+            var expectedData = new[] {24, 16};
+            _monitor.SentFromClientEvents.ShouldAllBeEquivalentTo(expectedData);
+        }
+
+        [Test]
+        public void RegisterSentFromClientEvent_should_be_able_to_handle_large_dataSets()
+        {
+            var values = GetDummyMillisecondValues(100, 1000);
+            RegisterSentFromClientEvents(values, 10);
+
+            var expectedData = new[] {99, 100, 100, 100, 100, 100, 100, 100, 100, 100, 1};
+            _monitor.SentFromClientEvents.ShouldAllBeEquivalentTo(expectedData);
         }
 
         private IEnumerable<long> GetDummyMillisecondValues(int eventInterval, int totalNumber)
         {
             var values = new List<long>();
+            var startLong = _monitor.StartTime.ToMilliseconds();
 
             for (var i = 0; i < totalNumber; i++)
             {
-                
+                var value = startLong + eventInterval;
+                startLong += eventInterval;
+                values.Add(value);
+            }
+
+            return values;
+        }
+
+        private void RegisterSentFromClientEvents(IEnumerable<long> values, int spacing = 1)
+        {
+            foreach (var value in values)
+            {
+                _monitor.RegisterSentFromClientEvent(value, spacing);
             }
         }
 
