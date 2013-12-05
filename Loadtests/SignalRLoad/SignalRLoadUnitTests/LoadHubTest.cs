b66aff0a187dccde490d8320a6f34a2cf4f99cca
kjohann <kris-90@live.no>
2013-12-05 19:45:31 +0100

    Wrote tests for echo

--- Loadtests/SignalRLoad/SignalRLoadUnitTests/LoadHubTest.cs
+++ Loadtests/SignalRLoad/SignalRLoadUnitTests/LoadHubTest.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
+using SignalRLoad.Extensions;
 using SignalRLoad.Models;
 using SignalRLoadUnitTests.Hubs;
 using FluentAssertions;
@@ -10,13 +12,17 @@ namespace SignalRLoadUnitTests
     {
         private TestableLoadHub _loadHub;
         private Monitor _monitor;
+        private Message _message;
 
         [SetUp]
         public void SetUp()
         {
             _loadHub = new TestableLoadHub();
-            _monitor = Monitor.GetInstance();
+            _monitor = Monitor.GetInstance();            
             _monitor.Reset();
+            _monitor.NumberOfClients = 100;
+            _monitor.StartTime = DateTime.UtcNow;
+            _message = new Message { SentFromClient = _monitor.StartTime.AddMilliseconds(50).ToMilliseconds() };
         }
 
         [Test]
@@ -35,6 +41,36 @@ namespace SignalRLoadUnitTests
             _monitor.NumberOfClients.ShouldBeEquivalentTo(numberOfClients);
         }
 
+        [Test]
+        public void Echo_should_set_ReceivedAtServer_in_message()
+        {
+            _loadHub.Echo(_message);
+            _message.ReceivedAtServer.Should().NotBe(0);
+        }
+
+        [Test]
+        public void Echo_should_register_a_ReceivedAtServerEvent_in_monitor()
+        {
+            _loadHub.Echo(_message);
+            var expected = new[] {1};
+            _monitor.ReceivedAtServerEvents.ShouldAllBeEquivalentTo(expected);
+        }
+
+        [Test]
+        public void Echo_should_register_a_SentFromClientEvent_in_monitor()
+        {
+            _loadHub.Echo(_message);
+            var expected = new[] {1};
+            _monitor.SentFromClientEvents.ShouldAllBeEquivalentTo(expected);
+        }
+
+        [Test]
+        public void Echo_should_register_a_SentFromServerEvent_in_monitor()
+        {
+            _loadHub.Echo(_message);
+            var expected = new[] {1};
+            _monitor.SentFromServerEvents.ShouldAllBeEquivalentTo(expected);
+        }
 
     }
 }
