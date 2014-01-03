6d193cc6ac6d24b5c7b686eaa9bfabcec584b461
kjohann <kris-90@live.no>
2014-01-03 13:44:15 +0100

    Wrote tests for harvestComplete

--- Loadtests/SignalRLoad/ClientTests/Tests/ClientFunctionsTests.js
+++ Loadtests/SignalRLoad/ClientTests/Tests/ClientFunctionsTests.js
@@ -1,1 +1,37 @@
 ﻿var should = chai.should();
+
+describe("Clientfunctions", function () {
+    it("harvestComplete should set masterId to 0 if it is not 0 already", function() {
+        var domStub = sinon.stub(loadTest.dom, "changeOnHarvestComplete");
+        loadTest.options.masterId = 1;
+        var dummyData = { data: "Testdata" };
+
+        loadTest.clientFunctions.harvestComplete(dummyData);
+
+        loadTest.options.masterId.should.equal(0);
+        domStub.restore();
+    });
+    it("harvestComplete should attach incoming data to loadTest object", function() {
+        var domStub = sinon.stub(loadTest.dom, "changeOnHarvestComplete");
+        loadTest.options.masterId = 1;
+        var dummyData = { data: "Testdata" };
+
+        loadTest.clientFunctions.harvestComplete(dummyData);
+
+        loadTest.data.should.equal(dummyData);
+        domStub.restore();
+    });
+    it("harvestComplete should not do any work if masterId is 0", function() {
+        var domMock = sinon.mock(loadTest.dom);
+        domMock.expects("changeOnHarvestComplete").atMost(0);
+        
+        loadTest.options.masterId = 0;
+        var dummyData = { data: "Testdata" };
+        
+        loadTest.clientFunctions.harvestComplete(dummyData);
+
+        domMock.verify();
+        domMock.restore();
+
+    });
+});
