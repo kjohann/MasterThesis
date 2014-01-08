6c94875b0610dd1d67631a22e73763455b3f9ef6
kjohann <kris-90@live.no>
2014-01-08 15:11:37 +0100

    Implemented monitor.reset with test

--- Loadtests/SocketIOLoad/Server/monitor.js
+++ Loadtests/SocketIOLoad/Server/monitor.js
@@ -1,1 +1,37 @@
+var Monitor = function() {
+    var self = this;
+    self.numberOfClients = 0;
+    self.completedClients = [];
+    self.duration = 0;
+    self.testDataEntities = [];
+    self.startTime = 0;
+    self.spacing = 0;
+    self.harvested = 0;
+    self.sentFromClientEvents = [];
+    self.receivedAtServerEvents = [];
+    self.sentFromServerEvents = [];
+};
+
+Monitor.prototype.reset = function() {
+    var self = this;
+    self.numberOfClients = 0;
+    self.completedClients = [];
+    self.duration = 0;
+    self.testDataEntities = [];
+    self.spacing = 0;
+    self.harvested = 0;
+    self.sentFromClientEvents = [];
+    self.receivedAtServerEvents = [];
+    self.sentFromServerEvents = [];
+}
+
+var instance = null;
+
+exports.getInstance = function() {
+    if(instance) {
+        return instance;
+    }
+    instance = new Monitor();
+    return instance;
+}
 
--- Loadtests/SocketIOLoad/Tests/monitorTest.js
+++ Loadtests/SocketIOLoad/Tests/monitorTest.js
@@ -1,7 +1,35 @@
-var monitor = require("../Server/monitor.js");
+var monitor = require("../Server/monitor.js").getInstance();
 var should = require('chai').should();
 
 describe("monitor", function() {
+    before(function() {
+        monitor.reset();
+        monitor.startTime = new Date("2014-1-8");
+        monitor.numberOfClients = 100;
+    });
+    it("reset sets all public fields of the monitor exept startTime back to inital values", function() {
+        monitor.completedClients.push("Something");
+        monitor.duration = 1000;
+        monitor.harvested = 10;
+        monitor.numberOfClients = 10;
+        monitor.receivedAtServerEvents.push(5);
+        monitor.sentFromClientEvents.push(5);
+        monitor.sentFromServerEvents.push(25);
+        monitor.spacing = 10;
+        monitor.testDataEntities.push({latencyData: [10,10]});
+
+        monitor.reset();
+
+        monitor.completedClients.length.should.equal(0);
+        monitor.duration.should.equal(0);
+        monitor.harvested.should.equal(0);
+        monitor.numberOfClients.should.equal(0);
+        monitor.receivedAtServerEvents.length.should.equal(0);
+        monitor.sentFromClientEvents.length.should.equal(0);
+        monitor.sentFromServerEvents.length.should.equal(0);
+        monitor.spacing.should.equal(0);
+        monitor.testDataEntities.length.should.equal(0);
+    });
     it("registerSentFromClientEvent should register an event within the correct interval", function() {
 
     });
