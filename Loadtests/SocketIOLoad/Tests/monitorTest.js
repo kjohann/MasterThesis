24e15aa4b4a1ab813c606d18d9b4a525970ae226
kjohann <kris-90@live.no>
2014-01-08 15:39:19 +0100

    Wrote the tests for registerSentFromClientEvent

--- Loadtests/SocketIOLoad/Tests/monitorTest.js
+++ Loadtests/SocketIOLoad/Tests/monitorTest.js
@@ -4,7 +4,7 @@ var should = require('chai').should();
 describe("monitor", function() {
     before(function() {
         monitor.reset();
-        monitor.startTime = new Date("2014-1-8");
+        monitor.startTime = new Date("2014-1-8").getTime();
         monitor.numberOfClients = 100;
     });
     it("reset sets all public fields of the monitor exept startTime back to inital values", function() {
@@ -31,22 +31,44 @@ describe("monitor", function() {
         monitor.testDataEntities.length.should.equal(0);
     });
     it("registerSentFromClientEvent should register an event within the correct interval", function() {
+        var values = getDummyMillisecondValues(200, 20);
+        registerSentFromClientEvents(values);
 
+        var expectedData = [4, 5, 5, 5, 1];
+
+        monitor.sentFromClientEvents.shouldAllBeEqual(expectedData);
     });
     it("registerSentFromClientEvent should register an event also with different spacing", function() {
+        var values = getDummyMillisecondValues(200, 20);
+        registerSentFromClientEvents(values, 5);
 
+        var expectedData = [24, 16];
+
+        monitor.sentFromClientEvents.shouldAllBeEqual(expectedData);
     });
     it("registerSentFromClientEvent should be abele to handle large data sets", function() {
+        var values = getDummyMillisecondValues(100, 1000);
+        registerSentFromClientEvents(values, 10);
 
+        var expectedData = [99, 100, 100, 100, 100, 100, 100, 100, 100, 100, 1];
+
+        monitor.sentFromClientEvents.shouldAllBeEqual(expectedData);
     });
     it("registerSentFromClientEvent should return the corresponding key for the event with spacing one", function() {
-
+        var startLong = monitor.startTime;
+        monitor.registerSentFromClientEvent(startLong + 999).should.equal(0);
     });
     it("registerSentFromClientEvent should return the corresponding key for the event with spacing more than one", function() {
-
+        var startLong = monitor.startTime;
+        monitor.registerSentFromClientEvent(startLong + 100000, 10).should.equal(10);
     });
     it("registerReceivedAtServerEvent should register an event within the correct interval", function() {
+        var values = getDummyMillisecondValues(200, 20);
+        registerReceivedAtServerEvents(values);
 
+        var expectedData = [4, 5, 5, 5, 1];
+
+        monitor.receivedAtServerEvents.shouldAllBeEqual(expectedData);
     });
     it("registerReceivedAtServerEvent should register an event also with different spacing", function() {
 
@@ -76,3 +98,47 @@ describe("monitor", function() {
 
     });
 });
+
+function getDummyMillisecondValues(eventInterval, totalNumber) {
+    var values = [];
+    var startLong = monitor.startTime;
+
+    for (var i = 0; i < totalNumber; i++)
+    {
+        var value = startLong + eventInterval;
+        startLong += eventInterval;
+        values.push(value);
+    }
+
+    return values;
+}
+
+function registerSentFromClientEvents(values, spacing) {
+    var sp = spacing ? spacing : 1;
+
+    for(var value in values) {
+        monitor.registerSentFromClientEvent(value, sp);
+    }
+}
+
+function registerReceivedAtServerEvents(values, spacing) {
+    var sp = spacing ? spacing : 1;
+
+    for(var value in values) {
+        monitor.registerReceivedAtServerEvent(value, sp);
+    }
+}
+
+Array.prototype.shouldAllBeEqual = function (array2) {
+    if(this.length != array2.length) {
+        this.length.should.equal(array2.length);
+        return;
+    }
+
+    for(var i = 0; i < this.length; i++) {
+        if(this[i] != array2[i]) {
+            console.log("Differ at index: " + i);
+        }
+        this[i].should.equal(array2[i]);
+    }
+}
