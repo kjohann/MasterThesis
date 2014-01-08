fd5431a4aa84bbd58c82aa3c1fa683412938ed0d
kjohann <kris-90@live.no>
2014-01-08 13:27:18 +0100

    Added skeleton for "hub" and monitor modules

---
+++ Loadtests/SocketIOLoad/Tests/hubTest.js
@@ -0,0 +1,48 @@
+var hub = require("../Server/hub.js");
+var should = require('chai').should();
+
+describe("hub", function() {
+    it("initTest resets monitor", function() {
+
+    });
+    it("initTest sets number of clients in monitor", function() {
+
+    });
+    it("initTest sets spacing in monitor", function() {
+
+    });
+    it("initTest sets incoming startTime in monitor", function() {
+
+    });
+    it("echo should set receivedAtServer in message", function() {
+
+    });
+    it("echo should register a receivedAtServerEvent in monitor", function() {
+
+    });
+    it("echo should register a sentFromServerEvent in monitor", function() {
+
+    });
+    it("echo should add key to message", function() {
+
+    });
+    it("broadcast should set receivedAtServer in message", function() {
+
+    });
+    it("broadcast should register a receivedAtServerEvent in monitor", function() {
+
+    });
+    it("broadcast should register a sentFromServerEvent in monitor", function() {
+
+    });
+    it("broadcast should add key to message", function() {
+
+    });
+    it("complete should add clientId to monitor completed list", function() {
+
+    });
+    it("complete should set duration in monitor if all clients have completed", function() {
+
+    });
+
+});
---
+++ Loadtests/SocketIOLoad/Tests/monitorTest.js
@@ -0,0 +1,50 @@
+var monitor = require("../Server/monitor.js");
+var should = require('chai').should();
+
+describe("monitor", function() {
+    it("registerSentFromClientEvent should register an event within the correct interval", function() {
+
+    });
+    it("registerSentFromClientEvent should register an event also with different spacing", function() {
+
+    });
+    it("registerSentFromClientEvent should be abele to handle large data sets", function() {
+
+    });
+    it("registerSentFromClientEvent should return the corresponding key for the event with spacing one", function() {
+
+    });
+    it("registerSentFromClientEvent should return the corresponding key for the event with spacing more than one", function() {
+
+    });
+    it("registerReceivedAtServerEvent should register an event within the correct interval", function() {
+
+    });
+    it("registerReceivedAtServerEvent should register an event also with different spacing", function() {
+
+    });
+    it("registerReceivedAtServerEvent should be able to handle large data sets", function() {
+
+    });
+    it("registerSentFromServerEvent should register an echo event within the correct interval", function() {
+
+    });
+    it("registerSentFromServerEvent should register a broadcast event within the correct interval", function() {
+
+    });
+    it("registerSentFromServerEvent should register an echo event also with different spacing", function() {
+
+    });
+    it("registerSentFromServerEvent should register a broadcast event also with different spacing", function() {
+
+    });
+    it("registerSentFromServerEvent should be able to handle large dataSets with echo", function() {
+
+    });
+    it("registerSentFromServerEvent should be able to handle large dataSets with broadcast", function() {
+
+    });
+    it("addEvent should fill in zero events if key points to an out of bounds index", function() {
+
+    });
+});
