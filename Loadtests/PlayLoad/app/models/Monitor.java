e9013cfb8fcd6c07706029decc1d0c3764d5d1a5
kjohann <kris-90@live.no>
2014-01-09 17:09:31 +0100

    Started writing tests for Monitor

--- Loadtests/PlayLoad/app/models/Monitor.java
+++ Loadtests/PlayLoad/app/models/Monitor.java
@@ -1,5 +1,29 @@
 package models;
 
+import java.util.List;
+import java.util.Set;
+
 public class Monitor {
+
+	public long startTime;
+	public int numberOfClients;
+	public List<TestDataEntity> testDataEntities;
+	public int spacing;
+	public List<Integer> sentFromServerEvents;
+	public List<Integer> rentFromClientEvents;
+	public List<Integer> receivedAtServerEvents;
+	public int harvested;
+	public int duration;
+	public Set<String> completedClients;
+
+	public Monitor getInstance() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void reset() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- Loadtests/PlayLoad/test/models/MonitorTest.java
+++ Loadtests/PlayLoad/test/models/MonitorTest.java
@@ -1,8 +1,34 @@
 package models;
 
+import java.util.Calendar;
+import java.util.List;
+
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class MonitorTest {
-
+	private Monitor _monitor;
+	
+	@Before
+	public void setUp() {
+		_monitor = _monitor.getInstance();
+		_monitor.reset();
+		Calendar c = Calendar.getInstance();
+		c.set(2014, 1, 9);
+		_monitor.startTime = c.getTimeInMillis();
+		_monitor.numberOfClients = 100;
+	}
+	
+	@Test
+	public void reset_sets_all_public_fields_of_the_monitor_exept_startTime_back_to_initial_values() {
+		_monitor.completedClients.add("Something");
+        _monitor.duration = 1000;
+        _monitor.harvested = 10;
+        _monitor.numberOfClients = 10;
+        _monitor.receivedAtServerEvents.add(5);
+        _monitor.rentFromClientEvents.add(5);
+        _monitor.sentFromServerEvents.add(25);
+        _monitor.spacing = 10;            
+        _monitor.testDataEntities.add(new TestDataEntity());
+	}
 }
