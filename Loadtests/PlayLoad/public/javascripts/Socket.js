08c9427e904c605b948054bf66347ba7d483a3f7
kjohann <kris-90@live.no>
2014-01-12 19:35:50 +0100

    Implemented WS part of Socket.js

--- Loadtests/PlayLoad/public/javascripts/Socket.js
+++ Loadtests/PlayLoad/public/javascripts/Socket.js
@@ -1,17 +1,51 @@
 ﻿(function(root, options) {
-    options.frameWork = null;
+    options.frameWork = "Play";
+    
+    function onMessage(json, self) {
+    	var response = JSON.parse(json);
+		if(response.messageKind === "initTest") {
+			self.functions[response.messageKind](response.testToRun);
+		} else if(response.messageKind === "receiveMessage") {
+			self.functions[response.messageKind](response.data);
+		} else if(response.messageKind === "harvest") {
+			self.functions[response.messageKind]();
+		} else if(response.messageKind === "harvestComplete") {
+			self.functions[response.messageKind]();
+		} else if(response.messageKind === "cid") {
+			self.cid = response.cid;
+		}
+    }
 
-    root.SocketInstance = function () {
+    root.SocketInstance = function (transport) {
         var self = this;
+        self.cid = 0;
+        self.functions = [];
+        self.commObj;
+        
+        if(!transport) {
+        	var WS =  window['MozWebSocket'] ? MozWebSocket : WebSocket;
+        	var url = "ws://localhost:9000/wsConnect";
+        	self.commObj = new WS(url);
+        	self.commObj.onmessage = function(data) {
+        		onMessage(data.data, self);
+        	};
+        } else {
+        	//comet
+        }
 
         self.bind = function(functionName, functionToCall) {
-            //Binds the function with name functionName to functionToCall  
+            self.functions[functionName] = functionToCall  
         };
 
         self.invoke = function() {
-            //can receive any number of arguments - to be used according to framework usage
             var args = Array.prototype.slice.call(arguments);
-
+            var data = {
+            	messageKind: args[0],
+            	cid: self.cid
+            };
+            args.splice(0,1);
+            data.data = args;
+            self.commObj.send(JSON.stringify(data));
         };
 
         self.start = function() {
