f5a50f5057b9356d47c5ef95249723f54e6831c3
kjohann <kris-90@live.no>
2014-01-13 20:58:56 +0100

    Added client files

---
+++ Loadtests/LightstreamerLoad/Client/Globals.js
@@ -0,0 +1,25 @@
+﻿(function() {
+    window.loadTest = window.loadTest || {
+        options: {
+            chartUrl: "http://localhost/ChartsAPI/api/charts",
+            spacing: 1,
+            masterId: 0, //will be 0 for all browsers that does not have the master
+            connectionInterval: 0, //delay between connecting clients 
+            numberOfClientsTotal: 0,
+            numberOfClientsPrBrowser: 0,
+            messageInterval: 0,
+            numberOfMessages: 0, //(pr. client) use this with messageInterval to calculate expected duration
+            instanceId: 1, //will be id of the first client
+            clients: [], //able to hold several clients
+            latencyEvents: [], //will hold accumulated latency within each interval (time)
+            registeredMessages: [],
+            connectionsTried: 0,
+            locks: {
+                initLock: 0,
+                harvestLock: 0,
+                allComplete: false
+}
+        },
+    };    
+    //example regarding instanceId and clients: 5 clients pr. browser: instanceId will be 1 and it will hold clients with ids 1, 2, 3, 4 and 5. Next instanceId will be 6
+})();
---
+++ Loadtests/LightstreamerLoad/Client/Main.js
@@ -0,0 +1,34 @@
+﻿(function(options, charts, comm, functions) {
+    $(function () {
+        $("#header").append(options.frameWork);
+        $("#getCharts").hide();
+        $("#running").hide();
+        $("#init").hide();
+        $("#type").hide();
+        $("#master").hide();
+        $("#msgInterval").hide();
+        $("#msgs").hide();
+        
+        $("#getCharts").click(function () {
+            charts.getCharts(loadTest.data);
+        });
+
+        $("#connect").click(function() {
+            options.connectionInterval = parseInt($("#connInterval").val());
+            options.numberOfClientsTotal = parseInt($("#numberOfClients").val());
+            options.numberOfClientsPrBrowser = parseInt($("#numberOfClients").val());
+            comm.initConnection();
+        });            
+
+        $("#init").click(function () {
+            var test = $("#type").val();
+            options.messageInterval = parseInt($("#msgInterval").val());
+            options.numberOfMessages = parseInt($("#msgs").val());
+            comm.start(test);
+        });
+
+        $("#masterBtn").click(function() {            
+            functions.promoteToMaster();
+        });
+    });
+})(loadTest.options, loadTest.charts, loadTest.communications, loadTest.clientFunctions)
