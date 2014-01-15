8819a7035b0c01c9e9feb854e0653a0d776761a3
kjohann <kris-90@live.no>
2014-01-15 16:30:55 +0100

    Made js files of the testData - don't need the json files for the unit tests

---
+++ Loadtests/ChartMerger/tests/data/LPJsonAsJSObj.js
@@ -0,0 +1,294 @@
+window.merger.lpObj = [{
+    "Framework": "Socket.IO",
+    "Timestamp": 1389785834672,
+    "Charts": [{
+        "Title": "Messages sent from clients and received by server pr. second",
+        "XAxis": ["0", "1", "2", "3", "4"],
+        "YAxis": null,
+        "Series": [{
+            "Data": [50, 50, 30, 50, 20],
+            "Name": "Received by server"
+        },
+            {
+                "Data": [50, 50, 30, 50, 20],
+                "Name": "Sent from clients"
+            }
+        ],
+        "YAxisTitle": "Messages"
+    },
+        {
+            "Title": "Messages sent from server pr. second",
+            "XAxis": ["0", "1", "2", "3", "4"],
+            "YAxis": null,
+            "Series": [{
+                "Data": [1000, 1000, 600, 1000, 400],
+                "Name": "Messages"
+            }
+            ],
+            "YAxisTitle": "Messages"
+        },
+        {
+            "Title": "Average Latency",
+            "XAxis": ["0", "1", "2", "3", "4"],
+            "YAxis": null,
+            "Series": [{
+                "Data": [110.10, 96.08, 263.64, 108.50, 108.50],
+                "Name": "Average latency (ms)"
+            }
+            ],
+            "YAxisTitle": "Average milliseconds"
+        }
+    ]
+},
+    {
+        "Framework": "Socket.IO",
+        "Timestamp": 1389785844672,
+        "Charts": [{
+            "Title": "Messages sent from clients and received by server pr. second",
+            "XAxis": ["0", "1", "2", "3", "4"],
+            "YAxis": null,
+            "Series": [{
+                "Data": [50, 50, 30, 50, 20],
+                "Name": "Received by server"
+            },
+                {
+                    "Data": [50, 50, 30, 50, 20],
+                    "Name": "Sent from clients"
+                }
+            ],
+            "YAxisTitle": "Messages"
+        },
+            {
+                "Title": "Messages sent from server pr. second",
+                "XAxis": ["0", "1", "2", "3", "4"],
+                "YAxis": null,
+                "Series": [{
+                    "Data": [1000, 1000, 600, 1000, 400],
+                    "Name": "Messages"
+                }
+                ],
+                "YAxisTitle": "Messages"
+            },
+            {
+                "Title": "Average Latency",
+                "XAxis": ["0", "1", "2", "3", "4"],
+                "YAxis": null,
+                "Series": [{
+                    "Data": [110.10, 96.08, 263.64, 108.50, 108.50],
+                    "Name": "Average latency (ms)"
+                }
+                ],
+                "YAxisTitle": "Average milliseconds"
+            }
+        ]
+    },
+    {
+        "Framework": "SignalR",
+        "Timestamp": 1389785834672,
+        "Charts": [{
+            "Title": "Messages sent from clients and received by server pr. second",
+            "XAxis": ["0", "1", "2", "3", "4", "5"],
+            "YAxis": null,
+            "Series": [{
+                "Data": [50, 50, 30, 50, 20, 20],
+                "Name": "Received by server"
+            },
+                {
+                    "Data": [50, 50, 30, 50, 20, 20],
+                    "Name": "Sent from clients"
+                }
+            ],
+            "YAxisTitle": "Messages"
+        },
+            {
+                "Title": "Messages sent from server pr. second",
+                "XAxis": ["0", "1", "2", "3", "4", "5"],
+                "YAxis": null,
+                "Series": [{
+                    "Data": [1000, 1000, 600, 1000, 400, 400],
+                    "Name": "Messages"
+                }
+                ],
+                "YAxisTitle": "Messages"
+            },
+            {
+                "Title": "Average Latency",
+                "XAxis": ["0", "1", "2", "3", "4"],
+                "YAxis": null,
+                "Series": [{
+                    "Data": [110.10, 96.08, 263.64, 108.50, 108.50, 108.50],
+                    "Name": "Average latency (ms)"
+                }
+                ],
+                "YAxisTitle": "Average milliseconds"
+            }
+        ]
+    },
+    {
+        "Framework": "Socket.IO",
+        "Timestamp": 1389785834672,
+        "Charts": [{
+            "Title": "Messages sent from clients and received by server pr. second",
+            "XAxis": ["0", "1", "2", "3", "4", "5"],
+            "YAxis": null,
+            "Series": [{
+                "Data": [50, 50, 30, 50, 20, 20],
+                "Name": "Received by server"
+            },
+                {
+                    "Data": [50, 50, 30, 50, 20, 20],
+                    "Name": "Sent from clients"
+                }
+            ],
+            "YAxisTitle": "Messages"
+        },
+            {
+                "Title": "Messages sent from server pr. second",
+                "XAxis": ["0", "1", "2", "3", "4", "5"],
+                "YAxis": null,
+                "Series": [{
+                    "Data": [1000, 1000, 600, 1000, 400, 400],
+                    "Name": "Messages"
+                }
+                ],
+                "YAxisTitle": "Messages"
+            },
+            {
+                "Title": "Average Latency",
+                "XAxis": ["0", "1", "2", "3", "4", "5"],
+                "YAxis": null,
+                "Series": [{
+                    "Data": [110.10, 96.08, 263.64, 108.50, 108.50, 108.50],
+                    "Name": "Average latency (ms)"
+                }
+                ],
+                "YAxisTitle": "Average milliseconds"
+            }
+        ]
+    },
+    {
+        "Framework": "Socket.IO",
+        "Timestamp": 1389785834672,
+        "Charts": [{
+            "Title": "Messages sent from clients and received by server pr. second",
+            "XAxis": ["0", "1", "2", "3", "4", "5", "6"],
+            "YAxis": null,
+            "Series": [{
+                "Data": [50, 50, 30, 50, 20, 20, 30],
+                "Name": "Received by server"
+            },
+                {
+                    "Data": [50, 50, 30, 50, 20, 20, 30],
+                    "Name": "Sent from clients"
+                }
+            ],
+            "YAxisTitle": "Messages"
+        },
+            {
+                "Title": "Messages sent from server pr. second",
+                "XAxis": ["0", "1", "2", "3", "4", "5", "6"],
+                "YAxis": null,
+                "Series": [{
+                    "Data": [1000, 1000, 600, 1000, 400, 400, 600],
+                    "Name": "Messages"
+                }
+                ],
+                "YAxisTitle": "Messages"
+            },
+            {
+                "Title": "Average Latency",
+                "XAxis": ["0", "1", "2", "3", "4"],
+                "YAxis": null,
+                "Series": [{
+                    "Data": [110.10, 96.08, 263.64, 108.50, 108.50, 108.50, 96.08],
+                    "Name": "Average latency (ms)"
+                }
+                ],
+                "YAxisTitle": "Average milliseconds"
+            }
+        ]
+    },
+    {
+        "Framework": "Play",
+        "Timestamp": 1389785834672,
+        "Charts": [{
+            "Title": "Messages sent from clients and received by server pr. second",
+            "XAxis": ["0", "1", "2", "3", "4"],
+            "YAxis": null,
+            "Series": [{
+                "Data": [50, 50, 30, 50, 20],
+                "Name": "Received by server"
+            },
+                {
+                    "Data": [50, 50, 30, 50, 20],
+                    "Name": "Sent from clients"
+                }
+            ],
+            "YAxisTitle": "Messages"
+        },
+            {
+                "Title": "Messages sent from server pr. second",
+                "XAxis": ["0", "1", "2", "3", "4"],
+                "YAxis": null,
+                "Series": [{
+                    "Data": [1000, 1000, 600, 1000, 400],
+                    "Name": "Messages"
+                }
+                ],
+                "YAxisTitle": "Messages"
+            },
+            {
+                "Title": "Average Latency",
+                "XAxis": ["0", "1", "2", "3", "4"],
+                "YAxis": null,
+                "Series": [{
+                    "Data": [110.10, 96.08, 263.64, 108.50, 108.50],
+                    "Name": "Average latency (ms)"
+                }
+                ],
+                "YAxisTitle": "Average milliseconds"
+            }
+        ]
+    },
+    {
+        "Framework": "Lightstreamer",
+        "Timestamp": 1389785834672,
+        "Charts": [{
+            "Title": "Messages sent from clients and received by server pr. second",
+            "XAxis": ["0", "1", "2", "3", "4"],
+            "YAxis": null,
+            "Series": [{
+                "Data": [50, 50, 30, 50, 20],
+                "Name": "Received by server"
+            },
+                {
+                    "Data": [50, 50, 30, 50, 20],
+                    "Name": "Sent from clients"
+                }
+            ],
+            "YAxisTitle": "Messages"
+        },
+            {
+                "Title": "Messages sent from server pr. second",
+                "XAxis": ["0", "1", "2", "3", "4"],
+                "YAxis": null,
+                "Series": [{
+                    "Data": [1000, 1000, 600, 1000, 400],
+                    "Name": "Messages"
+                }
+                ],
+                "YAxisTitle": "Messages"
+            },
+            {
+                "Title": "Average Latency",
+                "XAxis": ["0", "1", "2", "3", "4"],
+                "YAxis": null,
+                "Series": [{
+                    "Data": [110.10, 96.08, 263.64, 108.50, 108.50],
+                    "Name": "Average latency (ms)"
+                }
+                ],
+                "YAxisTitle": "Average milliseconds"
+            }
+        ]
+    }];
---
+++ Loadtests/ChartMerger/tests/data/WSJsonAsJSObj.js
@@ -0,0 +1,295 @@
+window.merger.wsOjb = [{
+    "Framework": "Socket.IO",
+    "Timestamp": 1389785834672,
+    "Charts": [{
+        "Title": "Messages sent from clients and received by server pr. second",
+        "XAxis": ["0", "1", "2", "3", "4"],
+        "YAxis": null,
+        "Series": [{
+            "Data": [100, 100, 60, 100, 40],
+            "Name": "Received by server"
+        },
+            {
+                "Data": [100, 100, 60, 100, 40],
+                "Name": "Sent from clients"
+            }
+        ],
+        "YAxisTitle": "Messages"
+    },
+        {
+            "Title": "Messages sent from server pr. second",
+            "XAxis": ["0", "1", "2", "3", "4"],
+            "YAxis": null,
+            "Series": [{
+                "Data": [2000, 2000, 1200, 2000, 800],
+                "Name": "Messages"
+            }
+            ],
+            "YAxisTitle": "Messages"
+        },
+        {
+            "Title": "Average Latency",
+            "XAxis": ["0", "1", "2", "3", "4"],
+            "YAxis": null,
+            "Series": [{
+                "Data": [55.05, 48.04, 131.82, 54.25, 54.25],
+                "Name": "Average latency (ms)"
+            }
+            ],
+            "YAxisTitle": "Average milliseconds"
+        }
+    ]
+},
+    {
+        "Framework": "Socket.IO",
+        "Timestamp": 1389785844672,
+        "Charts": [{
+            "Title": "Messages sent from clients and received by server pr. second",
+            "XAxis": ["0", "1", "2", "3", "4"],
+            "YAxis": null,
+            "Series": [{
+                "Data": [100, 100, 60, 100, 40],
+                "Name": "Received by server"
+            },
+                {
+                    "Data": [100, 100, 60, 100, 40],
+                    "Name": "Sent from clients"
+                }
+            ],
+            "YAxisTitle": "Messages"
+        },
+            {
+                "Title": "Messages sent from server pr. second",
+                "XAxis": ["0", "1", "2", "3", "4"],
+                "YAxis": null,
+                "Series": [{
+                    "Data": [2000, 2000, 1200, 2000, 800],
+                    "Name": "Messages"
+                }
+                ],
+                "YAxisTitle": "Messages"
+            },
+            {
+                "Title": "Average Latency",
+                "XAxis": ["0", "1", "2", "3", "4"],
+                "YAxis": null,
+                "Series": [{
+                    "Data": [55.05, 48.04, 131.82, 54.25, 54.25],
+                    "Name": "Average latency (ms)"
+                }
+                ],
+                "YAxisTitle": "Average milliseconds"
+            }
+        ]
+    },
+    {
+        "Framework": "SignalR",
+        "Timestamp": 1389785834672,
+        "Charts": [{
+            "Title": "Messages sent from clients and received by server pr. second",
+            "XAxis": ["0", "1", "2", "3", "4", "5"],
+            "YAxis": null,
+            "Series": [{
+                "Data": [100, 100, 60, 100, 40, 40],
+                "Name": "Received by server"
+            },
+                {
+                    "Data": [100, 100, 60, 100, 40, 40],
+                    "Name": "Sent from clients"
+                }
+            ],
+            "YAxisTitle": "Messages"
+        },
+            {
+                "Title": "Messages sent from server pr. second",
+                "XAxis": ["0", "1", "2", "3", "4", "5"],
+                "YAxis": null,
+                "Series": [{
+                    "Data": [2000, 2000, 1200, 2000, 800, 800],
+                    "Name": "Messages"
+                }
+                ],
+                "YAxisTitle": "Messages"
+            },
+            {
+                "Title": "Average Latency",
+                "XAxis": ["0", "1", "2", "3", "4"],
+                "YAxis": null,
+                "Series": [{
+                    "Data": [55.05, 48.04, 131.82, 54.25, 54.25, 54.25],
+                    "Name": "Average latency (ms)"
+                }
+                ],
+                "YAxisTitle": "Average milliseconds"
+            }
+        ]
+    },
+    {
+        "Framework": "Socket.IO",
+        "Timestamp": 1389785834672,
+        "Charts": [{
+            "Title": "Messages sent from clients and received by server pr. second",
+            "XAxis": ["0", "1", "2", "3", "4", "5"],
+            "YAxis": null,
+            "Series": [{
+                "Data": [100, 100, 60, 100, 40, 40],
+                "Name": "Received by server"
+            },
+                {
+                    "Data": [100, 100, 60, 100, 40, 40],
+                    "Name": "Sent from clients"
+                }
+            ],
+            "YAxisTitle": "Messages"
+        },
+            {
+                "Title": "Messages sent from server pr. second",
+                "XAxis": ["0", "1", "2", "3", "4", "5"],
+                "YAxis": null,
+                "Series": [{
+                    "Data": [2000, 2000, 1200, 2000, 800, 800],
+                    "Name": "Messages"
+                }
+                ],
+                "YAxisTitle": "Messages"
+            },
+            {
+                "Title": "Average Latency",
+                "XAxis": ["0", "1", "2", "3", "4", "5"],
+                "YAxis": null,
+                "Series": [{
+                    "Data": [55.05, 48.04, 131.82, 54.25, 54.25, 54.25],
+                    "Name": "Average latency (ms)"
+                }
+                ],
+                "YAxisTitle": "Average milliseconds"
+            }
+        ]
+    },
+    {
+        "Framework": "Socket.IO",
+        "Timestamp": 1389785834672,
+        "Charts": [{
+            "Title": "Messages sent from clients and received by server pr. second",
+            "XAxis": ["0", "1", "2", "3", "4", "5", "6"],
+            "YAxis": null,
+            "Series": [{
+                "Data": [100, 100, 60, 100, 40, 40, 60],
+                "Name": "Received by server"
+            },
+                {
+                    "Data": [100, 100, 60, 100, 40, 40, 60],
+                    "Name": "Sent from clients"
+                }
+            ],
+            "YAxisTitle": "Messages"
+        },
+            {
+                "Title": "Messages sent from server pr. second",
+                "XAxis": ["0", "1", "2", "3", "4", "5", "6"],
+                "YAxis": null,
+                "Series": [{
+                    "Data": [2000, 2000, 1200, 2000, 800, 800, 1200],
+                    "Name": "Messages"
+                }
+                ],
+                "YAxisTitle": "Messages"
+            },
+            {
+                "Title": "Average Latency",
+                "XAxis": ["0", "1", "2", "3", "4"],
+                "YAxis": null,
+                "Series": [{
+                    "Data": [55.05, 48.04, 131.82, 54.25, 54.25, 54.25, 48.04],
+                    "Name": "Average latency (ms)"
+                }
+                ],
+                "YAxisTitle": "Average milliseconds"
+            }
+        ]
+    },
+    {
+        "Framework": "Play",
+        "Timestamp": 1389785834672,
+        "Charts": [{
+            "Title": "Messages sent from clients and received by server pr. second",
+            "XAxis": ["0", "1", "2", "3", "4"],
+            "YAxis": null,
+            "Series": [{
+                "Data": [100, 100, 60, 100, 40],
+                "Name": "Received by server"
+            },
+                {
+                    "Data": [100, 100, 60, 100, 40],
+                    "Name": "Sent from clients"
+                }
+            ],
+            "YAxisTitle": "Messages"
+        },
+            {
+                "Title": "Messages sent from server pr. second",
+                "XAxis": ["0", "1", "2", "3", "4"],
+                "YAxis": null,
+                "Series": [{
+                    "Data": [2000, 2000, 1200, 2000, 800],
+                    "Name": "Messages"
+                }
+                ],
+                "YAxisTitle": "Messages"
+            },
+            {
+                "Title": "Average Latency",
+                "XAxis": ["0", "1", "2", "3", "4"],
+                "YAxis": null,
+                "Series": [{
+                    "Data": [55.05, 48.04, 131.82, 54.25, 54.25],
+                    "Name": "Average latency (ms)"
+                }
+                ],
+                "YAxisTitle": "Average milliseconds"
+            }
+        ]
+    },
+    {
+        "Framework": "Lightstreamer",
+        "Timestamp": 1389785834672,
+        "Charts": [{
+            "Title": "Messages sent from clients and received by server pr. second",
+            "XAxis": ["0", "1", "2", "3", "4"],
+            "YAxis": null,
+            "Series": [{
+                "Data": [100, 100, 60, 100, 40],
+                "Name": "Received by server"
+            },
+                {
+                    "Data": [100, 100, 60, 100, 40],
+                    "Name": "Sent from clients"
+                }
+            ],
+            "YAxisTitle": "Messages"
+        },
+            {
+                "Title": "Messages sent from server pr. second",
+                "XAxis": ["0", "1", "2", "3", "4"],
+                "YAxis": null,
+                "Series": [{
+                    "Data": [2000, 2000, 1200, 2000, 800],
+                    "Name": "Messages"
+                }
+                ],
+                "YAxisTitle": "Messages"
+            },
+            {
+                "Title": "Average Latency",
+                "XAxis": ["0", "1", "2", "3", "4"],
+                "YAxis": null,
+                "Series": [{
+                    "Data": [55.05, 48.04, 131.82, 54.25, 54.25],
+                    "Name": "Average latency (ms)"
+                }
+                ],
+                "YAxisTitle": "Average milliseconds"
+            }
+        ]
+    }];
+
