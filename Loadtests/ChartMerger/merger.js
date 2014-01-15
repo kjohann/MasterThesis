74330f95a3b183b0016ef01102ae017476f9dbd7
kjohann <kris-90@live.no>
2014-01-15 16:31:15 +0100

    Wrote arrangeData function

--- Loadtests/ChartMerger/merger.js
+++ Loadtests/ChartMerger/merger.js
@@ -1,10 +1,17 @@
 (function(root) {
-    root.getAllDataEntriesFromFramework = function(framework) {
+    root.arrangeData = function(allChartsArray) {
+        //returns an associative array where each
+        // key contains an array of the charts of the framework the key represents
+        var arranged = [];
+        for(var i = 0; i < allChartsArray.length; i++) {
+            var data = allChartsArray[i];
+            if(arranged[data.Framework] === undefined) {
+                arranged[data.Framework] = [];
+            }
 
-    };
+            arranged[data.Framework].push(data);
+        }
 
-    root.calculateAverageOfSeries = function(dataEntries, seriesName) {
-
-    };
-
+        return arranged;
+    }
 })(merger);
--- Loadtests/ChartMerger/tests/mergerTest.js
+++ Loadtests/ChartMerger/tests/mergerTest.js
@@ -1,5 +1,23 @@
 var should = chai.should();
+var wsObj = window.merger.wsOjb;
+var lpObj = window.merger.lpObj;
+var socketIO = "Socket.IO", signalR = "SignalR", play = "Play", ls = "Lightstreamer";
 
 describe("merger", function() {
+    it("arrangeData should return an associative array containing an array for each framework", function() {
+        var arr = merger.arrangeData(wsObj);
 
+        arr[socketIO].should.be.an.instanceOf(Array);
+        arr[signalR].should.be.an.instanceOf(Array);
+        arr[play].should.be.an.instanceOf(Array);
+        arr[ls].should.be.an.instanceOf(Array);
+    });
+    it("arrangeData should place all chart objects of each framework in each entry in the returned array", function() {
+        var arr = merger.arrangeData(lpObj);
+
+        arr[socketIO].length.should.equal(4);
+        arr[signalR].length.should.equal(1);
+        arr[play].length.should.equal(1);
+        arr[ls].length.should.equal(1);
+    });
 });
