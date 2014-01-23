439305fa80261dd287e1f2ab8cbfa49fb4174cb9
kjohann <kris-90@live.no>
2014-01-23 13:27:43 +0100

    Applied changes to Play

--- Loadtests/PlayLoad/public/javascripts/Models.js
+++ Loadtests/PlayLoad/public/javascripts/Models.js
@@ -6,14 +6,15 @@
         self.messages = [];
         self.master = false;
         self.messagesSent = 0;
+        self.ownMessagesReceived = 0;
         self.completed = false;
     };
 
     root.Message = function(payload, clientId, messagesSentByClient) {
         var self = this;
         self.SentFromClient = new Date().getTime();
-        self.ReceivedAtServer = new Date().getTime(); //Just to init
-        self.ReceivedAtClient = new Date().getTime(); //Just to init
+        self.ReceivedAtServer = 0; //Just to init
+        self.ReceivedAtClient = 0; //Just to init
         self.Payload = payload;
         self.ClientId = clientId;
         self.MessageId = "c:" + clientId + "m:" + messagesSentByClient;
