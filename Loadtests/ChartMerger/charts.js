d53351b1fc98266bc544f79dc93ed5753d3394c2
kjohann <kris-90@live.no>
2014-01-27 11:47:06 +0100

    Added support for displaying manual data charts for single framework

--- Loadtests/ChartMerger/charts.js
+++ Loadtests/ChartMerger/charts.js
@@ -1,7 +1,7 @@
 (function(root) {
-    root.displayCharts = function(charts) {
+    root.displayBrowserDataCharts = function(charts) {
         $.each(charts, function(index, chart) {
-            var highChart = getChart(chart);
+            var highChart = getBrowserDataChart(chart);
             if (chart.Title === "Messages sent from clients and received by server pr. second") {
                 $("#messagesSentReceivedChart").highcharts(highChart);
             } else if (chart.Title === "Messages sent from server pr. second") {
@@ -10,44 +10,93 @@
                 $("#averageLatencyChart").highcharts(highChart);
             }
         });
-
-        function getChart(chart) {
-            return {
-                title: {
-                    text: chart.Title,
-                    x: -20
-                },
-                xAxis: {
-                    categories: chart.XAxis
-                },
-                yAxis: {
-                    title: {
-                        text: chart.YAxisTitle
-                    },
-                    plotLines: [{
-                        width: 1,
-                        color: '#808080'
-                    }]
-                },
-                legend: {
-                    layout: 'vertical',
-                    align: 'right',
-                    verticalAlign: 'middle',
-                    borderWidth: 0
-                },
-                series: mapSeries(chart)
-            };
-        }
-
-        function mapSeries(chart) {
-            return $.map(chart.Series, function (value, index) {
-                return {
-                    data: $.map(value.Data, function (val, i) {
-                        return parseFloat(val);
-                    }),
-                    name: value.Name
-                };
-            });
-        }
     }
+
+    root.displayManualDataChart = function(charts) {
+        $.each(charts, function(index, chart) {
+            var highChart = getManualDataChart(chart);
+            if(chart.title === "Bytes sent/received") {
+                $("#bytesSentReceivedChart").highcharts(highChart);
+            } else if(chart.title === "Peak processor usage") {
+                $("#peakProcessorChart").highcharts(highChart);
+            } else if(chart.title === "Max memory consumtion") {
+                $("#maxMemoryConsumtionChart").highcharts(highChart);
+            }
+        });
+    }
+
+    function getBrowserDataChart(chart) {
+        return {
+            title: {
+                text: chart.Title,
+                x: -20
+            },
+            xAxis: {
+                categories: chart.XAxis
+            },
+            yAxis: {
+                title: {
+                    text: chart.YAxisTitle
+                },
+                plotLines: [{
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle',
+                borderWidth: 0
+            },
+            series: mapSeries(chart)
+        };
+    }
+
+    function getManualDataChart(chart) {
+        return {
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: chart.title
+            },
+            xAxis: {
+                categories: chart.xAxis
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: chart.yAxisTitle
+                }
+            },
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                    '<td style="padding:0"><b>{point.y:.1f}' + chart.unit + '</b></td></tr>',
+                footerFormat: '</table>',
+                shared: true,
+                useHTML: true
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
+            },
+            series: chart.series
+        };
+    }
+
+    function mapSeries(chart) {
+        return $.map(chart.Series, function (value, index) {
+            return {
+                data: $.map(value.Data, function (val, i) {
+                    return parseFloat(val);
+                }),
+                name: value.Name
+            };
+        });
+    }
+
 })(merger.charts = merger.charts || {});
--- Loadtests/ChartMerger/main.js
+++ Loadtests/ChartMerger/main.js
@@ -1,39 +1,59 @@
 (function(merger, util, chartsHelper) {
     $(function(){
-        $("#fileInput").bind("change", function() {
+        $("#browserFileInput").bind("change", function() {
             util.readFileAsJson(this.files[0]).done(function(data) {
-                merger.rawData = data;
+                merger.rawBrowserData = data;
             }).fail(function(msg) {
                 console.log(msg);
             });
 
         });
 
+        $("#manualFileInput").bind("change", function() {
+            util.readFileAsJson(this.files[0]).done(function(data) {
+                merger.rawManualData = data;
+            }).fail(function(msg) {
+                    console.log(msg);
+            });
+        });
+
         $("#first").click(function() {
-            if(!merger.rawData) {
-                alert("Select file!");
-                return;
-            }
             var frameworks = $("#frameworks").val();
             if(!frameworks) {
                 alert("Select a framework!");
+                return;
             }
-            var charts = merger.getAverageChartsOfSingleFramework(frameworks[0], merger.rawData, merger.spacing);
 
-            chartsHelper.displayCharts(charts);
+            if(!merger.rawBrowserData) {
+                console.log("No browser data file selected");
+            } else {
+                var charts = merger.getAverageChartsOfSingleFramework(frameworks[0], merger.rawBrowserData, merger.spacing);
+                chartsHelper.displayBrowserDataCharts(charts);
+            }
+            var transports = $("#transports").val();
+            if(!merger.rawManualData) {
+                console.log("No manual data file selected");
+            } else {
+                var charts = [];
+                charts.push(merger.getManualDataChart("Bytes sent/received", [frameworks[0]], transports, "Bytes", merger.rawManualData));
+                charts.push(merger.getManualDataChart("Peak processor usage", [frameworks[0]], transports, "%", merger.rawManualData));
+                charts.push(merger.getManualDataChart("Max memory consumtion", [frameworks[0]], transports, "Bytes", merger.rawManualData));
+
+                chartsHelper.displayManualDataChart(charts)
+            }
         });
 
         $("#all").click(function() {
-            if(!merger.rawData) {
+            if(!merger.rawBrowserData) {
                 alert("Select file!");
                 return;
             }
 
             var frameworks = $("#frameworks").val();
 
-            var charts = merger.getAverageChartsCombined(merger.rawData, merger.spacing, frameworks);
+            var charts = merger.getAverageChartsCombined(merger.rawBrowserData, merger.spacing, frameworks);
 
-            chartsHelper.displayCharts(charts);
+            chartsHelper.displayBrowserDataCharts(charts);
         });
     });
 })(merger, merger.util, merger.charts);
